448c71f3d5ab545950af72b6fd9b3b7e328ef117
Josh Berryman <the.real.josh.berryman@gmail.com>
2013-02-18 12:28:46 +0100

    still tidying Lees-Edwards tests

--- testsuite/lees_edwards.tcl
+++ testsuite/lees_edwards.tcl
@@ -1,20 +1,39 @@
 
+#   Max-Planck-Institute for Polymer Research, Theory Group
+#  
+# This file is part of ESPResSo.
+#  
+# ESPResSo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#  
+# ESPResSo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
+# 
+#
+# To generate the test system, use gen_lees_edwards.tcl.
+#
+source "tests_common.tcl"
+
+require_feature "LEES_EDWARDS" on
+
+puts "------------------------------------------"
+puts "- Testcase lees_edwards.tcl running on [format %02d [setmd n_nodes]] nodes: -"
+puts "------------------------------------------"
 
 
-####reduced units are:
-## energy:   kcal/mol
-## distance: Angstrom
-## time:     
-
-##################################################
 # system setup
-set file "harm_system.data.gz"
+set L                 30     ;#box size
+set temperature        1.0   ;#temperature in reduced units
+set T                500     ;#temperature in Kelvin
 
-set L          30
-
-###kB in kcal/mol/K is 0.00198721
-set temperature 1.0   ;#temperature in reduced units
-set T         500     ;#temperature in Kelvin
 set k_temp [expr $T * 0.00198721]
 set k_b    [expr $k_temp / $temperature]
 set mass  1.67e-27        ; # to convert mass from AMU to kg
@@ -37,7 +56,7 @@ set n_part [expr $length_polymer * $N_chains]
 set lj_epsilon [expr  0.112/$k_temp]    
 
 #############################################################
-#  Lennard Jones Parameter                                  #
+#  Lennard Jones Parameters                                 #
 #############################################################
 set sigma 4.01
 set lj1_sig 1.0
@@ -55,37 +74,23 @@ setmd box_l $L $L $L
 setmd time_step $time_step
 setmd skin $skin
 
-set therm_1  1000
-set therm_2  7000
-set therm_3 10000
-
-
-#############################################################
-# Units conversion                                          #
-#############################################################
-set fac 6.94e-21        ; # To convert energy from Kcal/mol to Joules
-set length_conversion   [expr $sigma * 10e-10]
-set time_conversion     [expr $length_conversion * $time_step * sqrt($mass/($T*$k_b*$fac))] 
-set velocity_conversion [expr sqrt($fac * 2 * $mass)/$mass ]
-
-puts "#time     conversion factor to   s: $time_conversion"
-puts "#velocity conversion factor to m/s: $velocity_conversion"
+# Seed the Random Number Generator
+set ran_seed 1902
+set cmd "t_random seed"
+for {set i 0} {$i < [setmd n_nodes]} { incr i } { 
+   lappend cmd [expr $ran_seed + $i] 
+}
+eval $cmd
+expr srand($ran_seed)
 
 
 #############################################################
 #  Interaction Setup                                        #
 #############################################################
-# set up harm interaction
-#inter 0 harm $harm_k $harm_r0
 
 inter 0 0 lennard-jones $lj_epsilon $lj1_sig $lj1_cut auto 0  
-#inter 0 1 lennard-jones $lj2_epsilon $lj1_sig $lj1_cut auto 0  
-#inter 1 1 lennard-jones $lj3_epsilon $lj1_sig $lj1_cut auto 0 
-  
-# set up polymer
-#polymer $N_chains $length_polymer $harm_r0 mode RW bond 0
 
-# Particle types (0,1)
+# Place particles randomly
 for { set i 0 } { $i < $n_part } { incr i } {
     part $i pos [expr rand()*$L] [expr rand()*$L] [expr rand()*$L]
     part $i type 0
@@ -94,33 +99,25 @@ for { set i 0 } { $i < $n_part } { incr i } {
 ##################################################
 # simulation
 
-# run a number of integration steps
-puts "#thermalising:"
-
-set stepCount 0
-set step    100
-puts "##step || 2x(mean KE) || T?"
-
-#lees_edwards_offset  0.0
-#puts [lees_edwards_offset  7.5]
-
 ##start by shearing the system, and see what happens
 set max_step_shear 1000000
-set shear_equil      10000
+set shear_equil       5000
+set mean_from         1000
 set shear_per            1
 set write_per          100
-set therm_per           10
-set shear_rate           0.01
+set shear_rate           0.005
 set offset               0.0
 
 set f [open "thermalising.vtf" w]
 writevsf $f
 
-
 thermostat langevin $temperature 5.0
 inter forcecap  10
-integrate 100
-
+puts "#Shear displacement   |  <vx^2>    <vy^2>   <vz^2>"
+set mean_x2 0.0
+set mean_y2 0.0
+set mean_z2 0.0
+set count   0.0
 for { set step 0 } { $step < $shear_equil } { incr step $shear_per } {
     if { [expr $step % $write_per] == 0 } then {
         writevcf $f folded
@@ -133,11 +130,9 @@ for { set step 0 } { $step < $shear_equil } { incr step $shear_per } {
         set vx2 0.0
         set vy2 0.0
         set vz2 0.0
-        set mX  0.0
         for { set i 0 } { $i < $n_part } { incr i } {
             set pos [ part $i print pos ] 
             set vel [ part $i print v ] 
-            set mX  [expr $mX + [expr [lindex $vel 0]*([lindex $pos 1]-0.5*$L)]] 
             set vx2 [expr $vx2 + [expr [lindex $vel 0] * [lindex $vel 0]]] 
             set vy2 [expr $vy2 + [expr [lindex $vel 1] * [lindex $vel 1]]] 
             set vz2 [expr $vz2 + [expr [lindex $vel 2] * [lindex $vel 2]]] 
@@ -148,19 +143,34 @@ for { set step 0 } { $step < $shear_equil } { incr step $shear_per } {
         set KEx             [expr 2.0 * $vx2 / (3 * $n_part )]
         set KEy             [expr 2.0 * $vy2 / (3 * $n_part )]
         set KEz             [expr 2.0 * $vz2 / (3 * $n_part )]
-        set mX              [expr $mX / $n_part ]
 
-        puts "offset: $offset temp: $KEx $KEy $KEz $mX"
+        if { $step >= $mean_from } {
+            set mean_x2 [expr $mean_x2 + $KEx ] 
+            set mean_y2 [expr $mean_y2 + $KEy ] 
+            set mean_z2 [expr $mean_z2 + $KEz ] 
+            set count   [expr $count + 1.0 ] 
+        }
+
+        puts "$offset     $KEx $KEy $KEz"
         
     }
+
     set offset [expr $offset + $shear_rate * $shear_per]
-    invalidate_system
 }
 close $f
+
+set mean_x2 [expr $mean_x2 / $count ] 
+set mean_y2 [expr $mean_y2 / $count ] 
+set mean_z2 [expr $mean_z2 / $count ] 
+puts "#means: $mean_x2 $mean_y2 $mean_z2"
+
+##Below is the loop for an extended test, to
+##see if the code is stable without a forcecap for a long time.
+##exit
+
 set f [open "noCap.vtf" w]
 writevsf $f
-inter ljforcecap  0
-set write_per    10
+inter forcecap  0
 for { set step 0 } { $step < $max_step_shear } { incr step $shear_per } {
     if { [expr $step % $write_per] == 0 } then {
         writevcf $f folded
@@ -172,11 +182,9 @@ for { set step 0 } { $step < $max_step_shear } { incr step $shear_per } {
         set vx2 0.0
         set vy2 0.0
         set vz2 0.0
-        set mX  0.0
         for { set i 0 } { $i < $n_part } { incr i } {
             set pos [ part $i print pos ] 
             set vel [ part $i print v ] 
-            set mX  [expr $mX + [expr [lindex $vel 0]*([lindex $pos 1]-0.5*$L)]] 
             set vx2 [expr $vx2 + [expr [lindex $vel 0] * [lindex $vel 0]]] 
             set vy2 [expr $vy2 + [expr [lindex $vel 1] * [lindex $vel 1]]] 
             set vz2 [expr $vz2 + [expr [lindex $vel 2] * [lindex $vel 2]]] 
@@ -187,13 +195,11 @@ for { set step 0 } { $step < $max_step_shear } { incr step $shear_per } {
         set KEx             [expr 2.0 * $vx2 / (3 * $n_part )]
         set KEy             [expr 2.0 * $vy2 / (3 * $n_part )]
         set KEz             [expr 2.0 * $vz2 / (3 * $n_part )]
-        set mX              [expr $mX / $n_part ]
 
-        puts "offset: $offset temp: $KEx $KEy $KEz $mX"
+        puts "$offset     $KEx $KEy $KEz"
         
     }
     set offset [expr $offset + $shear_rate * $shear_per]
-    invalidate_system
 }
 
 
